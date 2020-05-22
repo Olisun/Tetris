0dd133e718008346b4d81d8b7ecdb8b7a810f5e6
Oliver Sun <olisun@mac.com>
2020-05-21 21:00:11 -0700

    created an array of divs to work with in app.js

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
+  const grid = document.querySelector('.grid');
+  // Turning all the sqaures into an array. 
+  let squares = Array.from(document.querySelectorAll('.grid div'));
+  const ScoreDisplay = document.getElementById('score');
+  const StartButton = document.getElementById('start');
+  const width = 10;
 
+  console.log(squares)
+
+  squares.forEach(square => {
+
+  })
+
+  showAlert('Oliver');
 
 })
