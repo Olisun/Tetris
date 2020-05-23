733adc5f3d917a4aefe5ad208399e5d9a9c7aa22
Oliver Sun <olisun@mac.com>
2020-05-22 20:48:32 -0700

    fixed grid in css. added shapes for the first 2 tetrominos in app.js

--- app.js
+++ app.js
@@ -2,16 +2,26 @@ document.addEventListener('DOMContentLoaded', () => {
   const grid = document.querySelector('.grid');
   // Turning all the sqaures into an array. 
   let squares = Array.from(document.querySelectorAll('.grid div'));
-  const ScoreDisplay = document.getElementById('score');
-  const StartButton = document.getElementById('start');
+  const ScoreDisplay = document.querySelector('#score');
+  const StartButton = document.querySelector('#start');
   const width = 10;
 
-  console.log(squares)
+  // The tetris blocks.  See TUT for explanation for how they were drawn in Excel. 
+  const lTetromino = [
+    [1, width + 1, width * 2 + 1, 2],
+    [width, width + 1, width + 2, width * 2 + 2],
+    [1, width + 1, width * 2 + 1, width * 2],
+    [width, width * 2, width * 2 + 1, width * 2 + 2]
+  ]
+
+  const zTetromino = [
+    [0, width, width + 1, width * 2 + 1],
+    [width + 1, width + 2, width * 2, width * 2 + 1],
+    [0, width, width + 1, width * 2 + 1],
+    [width + 1, width + 2, width * 2, width * 2 + 1]
+  ]
 
   squares.forEach(square => {
 
   })
-
-  showAlert('Oliver');
-
 })
